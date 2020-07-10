bb901f159f82257f900d83dc96711941b2f7523d
Ishween Kaur <31392376+ishween@users.noreply.github.com>
2020-07-10 18:18:08 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+from twilio.rest import Client
+
+
+# Your Account Sid and Auth Token from twilio.com/console
+# DANGER! This is insecure. See http://twil.io/secure
+account_sid = os.environ['account_sid']
+auth_token = os.environ['auth_token']
+whatsapp_no = os.environ['whatsapp_no']
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+                              body='Hello there!',
+                              from_='whatsapp:+14155238886',
+                              to='whatsapp:'+whatsapp_no
+                          )
